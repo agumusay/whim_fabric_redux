269605b18462991db8a812deac335946a96e0cab
agumusay <agumusay@protonmail.com>
2020-10-26 10:17:12 +0100

    fabric-drag-drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, createRef } from 'react';
 import logo from './logo.svg';
 import { Canvas } from './features/fabricCanvas/canvas';
 import { useSelector, useDispatch } from 'react-redux';
-import useDragging from './components/draggableComponent';
 import { canvasChange } from './features/fabricCanvas/canvasSlice';
 import { fabric } from 'fabric';
 import './App.css';
-import { Camera } from 'react-feather';
+
 let fabricCanvas = new fabric.Canvas();
 var listOfImages = [];
 
 function App() {
-	const [findImage, setFindImage] = useState(null);
 	const canvasState = useSelector(canvasChange);
-
-	const imageRefs = useRef([]);
-
 	const dispatch = useDispatch();
-	// const [dragRef, x, y, isDragging] = useDragging();
+	const [findImage, setFindImage] = useState(null);
+	const imageRefs = [];
+
 	let fabRef = useRef(React.createRef());
 	const importAll = r => {
 		return r.keys().map(r);
@@ -29,11 +26,10 @@ function App() {
 	};
 
 	const handleDragStart = ev => {
-		console.log(imageRefs.current[findImage]);
-		const { offsetTop, offsetLeft } = imageRefs.current[findImage].current;
-
-		imageOffsetX = ev.clientX - offsetLeft;
-		imageOffsetY = ev.clientY - offsetTop;
+		console.log(imageRefs[findImage].current.getBoundingClientRect());
+		const { top, left } = imageRefs[findImage].current.getBoundingClientRect();
+		imageOffsetX = ev.clientX - left;
+		imageOffsetY = ev.clientY - top;
 	};
 	const handleDragOver = ev => {
 		if (ev.preventDefault) {
@@ -50,24 +46,30 @@ function App() {
 		ev.preventDefault();
 		ev.stopPropagation();
 		console.log(findImage);
-		let img = imageRefs.current[findImage].current;
+		let img = imageRefs[findImage].current;
 
 		let offset = fabricCanvas._offset;
+		console.log(imageOffsetX, imageOffsetY);
 		let y = ev.clientY - (offset.top + imageOffsetY);
 		let x = ev.clientX - (offset.left + imageOffsetX);
-		let svgObj = null;
-		fabric.loadSVGFromURL(img.src, function (objects, options) {
-			let obj = fabric.util.groupSVGElements(objects, options);
-			fabricCanvas.add(obj).renderAll();
+
+		fabric.loadSVGFromURL(img.src, (objects, options) => {
+			let svgImage = fabric.util.groupSVGElements(objects, options);
+			svgImage.set({
+				left: x,
+				top: y,
+			});
+			svgImage.scale(0.3);
+			fabricCanvas.add(svgImage).renderAll();
 		});
-		let newImage = new fabric.Image(img, {
-			width: img.width,
-			height: img.height,
-			left: x,
-			top: y,
-		});
-		fabricCanvas.add(newImage);
-		fabricCanvas.renderAll();
+		// let newImage = new fabric.Image(img, {
+		// 	width: img.width,
+		// 	height: img.height,
+		// 	left: x,
+		// 	top: y,
+		// });
+		// fabricCanvas.add(newImage);
+		// fabricCanvas.renderAll();
 		return false;
 	};
 	listOfImages = importAll(require.context('../public/images/', false, /\.(png|jpe?g|svg)$/));
@@ -76,30 +78,36 @@ function App() {
 		let fabricCanvasRef = fabRef.current;
 
 		fabricCanvas.initialize(fabricCanvasRef, {
-			height: 1248,
-			width: 2000,
+			height: 947,
+			width: 1400,
 			backgroundColor: 'pink',
 		});
 
 		fabricCanvas.renderAll();
 	}, []);
-	imageRefs.current = listOfImages.map((ref, index) => (imageRefs.current[index] = React.createRef()));
+
 	return (
 		<div className='App'>
-			<div className='images-tab' style={{ display: 'flex', flexWrap: 'wrap', overflowY: 'scroll' }}>
-				{listOfImages.map((image, index) => (
-					<img
-						id={index}
-						ref={imageRefs.current[index]}
-						key={index}
-						src={image}
-						style={{}}
-						draggable='true'
-						onDragStart={handleDragStart}
-						onDragEnd={handleDragEnd}
-						onMouseDown={handleMouseDown}
-						alt=''></img>
-				))}
+			<div
+				className='images-tab'
+				style={{ display: 'flex', flexWrap: 'wrap', overflowY: 'scroll', justifyContent: 'center' }}>
+				{listOfImages.map((image, index) => {
+					const newRef = createRef();
+					imageRefs.push(newRef);
+					return (
+						<img
+							id={index}
+							ref={newRef}
+							key={index}
+							src={image}
+							style={{ width: 50, height: 50, margin: '2px' }}
+							draggable='true'
+							onDragStart={handleDragStart}
+							onDragEnd={handleDragEnd}
+							onMouseDown={handleMouseDown}
+							alt=''></img>
+					);
+				})}
 			</div>
 			<div className='canvasContainer' onDragEnter={handleDragEnter} onDrop={handleDrop} onDragOver={handleDragOver}>
 				<canvas ref={fabRef}></canvas>
